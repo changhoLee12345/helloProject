7b60de685768e211c2d081c227a381a917c540c7
admin <admin@DESKTOP-10KJD0R>
2023-06-01 08:54:52 +0900

    s

--- HelloApp/src/com/yedam/reference/ReferenceEx3.java
+++ HelloApp/src/com/yedam/reference/ReferenceEx3.java
@@ -6,16 +6,20 @@ public class ReferenceEx3 {
 		double[] scores = { 89.5, 78.9, 90.4 };
 		scores = new double[] { 55.5, 66.6, 77.7, 88.8 };
 		double maxVal = 0;
-		// 최소점수도 반영.
+		double minVal = 100;
+		// 최저점수도 반영.
 		double sum = 0;
 		for (double score : scores) {
 			if (maxVal < score) {
-				maxVal = score;
+				maxVal = score; // 최대점수
+			}
+			if (minVal > score) {
+				minVal = score; // 최저점수
 			}
 			sum += score;
 		}
 		double avg = sum / 3;
-		String fmt = "합계점수는 %.2f, 평균은 %.2f, 최고점수는 %.1f";
-		System.out.printf(fmt, sum, avg, maxVal);
+		String fmt = "합계점수는 %.2f, 평균은 %.2f, 최고점수는 %.1f, 최저점수는 %.1f";
+		System.out.printf(fmt, sum, avg, maxVal, minVal);
 	}
 }
